e061d87090fdeb0ba4b44d22be2cbaafb014f358
user <user@rrcs-172-254-99-32.nyc.biz.rr.com>
2020-02-16 21:25:43 +0900

    Updates

---
+++ precache-manifest.5c510bce7ad407accdc6c4aa9c019f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80e113ef678b2d99753daa4e8ee644e",
+    "url": "/quiz-app2/index.html"
+  },
+  {
+    "revision": "7b644ddcfb7bdfcb1fbf",
+    "url": "/quiz-app2/static/css/main.0b5daa81.chunk.css"
+  },
+  {
+    "revision": "52bac1a0876c370a022c",
+    "url": "/quiz-app2/static/js/2.c37657f7.chunk.js"
+  },
+  {
+    "revision": "030c497c4723ea4a0e1875f6555f517a",
+    "url": "/quiz-app2/static/js/2.c37657f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b644ddcfb7bdfcb1fbf",
+    "url": "/quiz-app2/static/js/main.d079f60d.chunk.js"
+  },
+  {
+    "revision": "1195864a2b3ae7d9204d",
+    "url": "/quiz-app2/static/js/runtime-main.bf7c87c6.js"
+  }
+]);
